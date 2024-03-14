7c2221b1a78163e41aca973a5e1273ca1384a28e
shivanikadali <shivanikadali2@gmail.com>
2024-03-14 21:07:24 +0530

    first commit

---
+++ src/main/java/demo/Main.java
@@ -0,0 +1,9 @@
+package demo;
+
+import javax.jms.JMSException;
+
+public class Main {
+    public static void main(String[] args) throws JMSException {
+        MessageReceiver.receivingFromQueue();
+    }
+}
---
+++ src/main/java/demo/MessageReceiver.java
@@ -0,0 +1,36 @@
+package demo;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+public class MessageReceiver {
+    private static String url = ActiveMQConnection.DEFAULT_BROKER_URL;
+    private static String queueName = "MESSAGE_QUEUE";
+
+    public static void receivingFromQueue() throws JMSException {
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(url);
+        Connection connection = connectionFactory.createConnection();
+        connection.start();
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        Destination destination = session.createQueue(queueName);
+        // messageconsumer is used for receiving
+        MessageConsumer consumer = session.createConsumer(destination);
+        // we receive the message
+        Message message = consumer.receive();
+
+        if (message instanceof TextMessage testMessage) {
+            //TextMessage testMessage=(TestMessage) message;
+            System.out.println("Received the message ' " + testMessage.getText() + "'");
+        }
+        connection.close();
+    }
+}
